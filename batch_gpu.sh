d9f0d8edd8c39e8768d11f06c896251ca76e6669
Monica Li <monica.yc.li@gmail.com>
2018-05-05 18:35:49 -0400

    update GPU, CPU, and node settings

--- batch_gpu.sh
+++ batch_gpu.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 #SBATCH --mail-type=ALL                 # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=monica.li@uconn.edu # Your email address
-#SBATCH --nodes=1                       # OpenMP requires a single node
-#SBATCH --ntasks=1                      # Run a single serial task
-#SBATCH --time=01:00:00                 # Time limit hh:mm:ss
+#SBATCH --nodes=1                       # Number of nodes
+#SBATCH --ntasks=1                      # Number of CPU cores per node
+#SBATCH --time=04:00:00                 # Time limit hh:mm:ss
 #SBATCH -e error_%A_%a.log              # Standard error
 #SBATCH -o output_%A_%a.log             # Standard output
 #SBATCH --job-name=TestTensorflow       # Descriptive job name
 #SBATCH --partition=gpu
-#SBATCH --gres=gpu:1                    # Request a GPU
+#SBATCH --gres=gpu:1                    # Request a GPU; max is 2
 
-export OMP_NUM_THREADS=1			#<= cpus-per-task
-export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=1	#<= cpus-per-task
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK    #<= cpus-per-task
 ##### END OF JOB DEFINITION  #####
 
 #Define user paths
@@ -23,9 +22,9 @@ export DIR_DATA=${DIR_BASE}/data             #rw
 export DIR_SCRIPTS=${DIR_BASE}/scripts       #ro, prepended to PATH
 export DIR_WORK=/work                        #rw /work on HPC is 40Gb local storage
 
-
 # Load modules
 module load singularity/2.3.1-gcc  #required to run the container
+modual load cuda/9.1
 
 # finally call the container with any arguments for the job
 # wrapper will bind the appropriate paths
