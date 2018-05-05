1bd5eebeb45304297fa3b985733e79e38bf51345
Monica Li <monica.yc.li@gmail.com>
2018-05-05 18:56:28 -0400

    add build_image.sh
    build singularity image from docker image

---
+++ HPC_scripts/00_build_image.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --mail-type=ALL                 # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=monica.li@uconn.edu # Your email address
+#SBATCH --nodes=1                       # Number of nodes
+#SBATCH --ntasks=1                      # Number of CPU cores per node
+#SBATCH --mem=1024mb                    # Memory limit
+#SBATCH --time=01:00:00                 # Time limit hh:mm:ss
+#SBATCH -e error_%A_%a.log              # Standard error
+#SBATCH -o output_%A_%a.log             # Standard output
+#SBATCH --job-name=BuildImage           # Descriptive job name
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1                    # Request a GPU; max is 2
+
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK      #<= cpus-per-task
+##### END OF JOB DEFINITION  #####
+
+# Load modules
+module load singularity/2.3.1-gcc
+module load cuda/9.1
+
+# build singularity image from docker image online
+mkdir -p /scratch/$USER/containers
+cd /scratch/$USER/containers
+singularity create --size 4000 compswr.img
+singularity import compswr.img --nv docker://monicaycli/compswr:latest
