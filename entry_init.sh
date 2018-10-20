7ae39ed8a860630dd859cb6746643f6d7a55a5cf
Monica Li <monica.yc.li@gmail.com>
2018-10-19 23:27:04 -0400

    update bash prompt and singularity compatibility

--- entry_init.sh
+++ entry_init.sh
@@ -2,12 +2,5 @@
 # entrypoint pre-initialization
 source /environment
 
-# bash prompt
-## \u user name
-## \h host name
-## COMPSWR project name, colored in cyan
-## \w working directory
-export PS1="\u@\h \[\e[1;36m\]COMPSWR\[\e[m\] \w\$ "
-
 # run the user command
 exec "$@"
